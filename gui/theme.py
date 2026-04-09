4682203bdbe13782ee5185f0736a5920e0c8e358
Advait <advaitture2@gmail.com>
2026-04-09 22:02:41 +0530

    Add files via upload

---
+++ gui/theme.py
@@ -0,0 +1,11 @@
+#Theme colours
+
+color_base = "#171717"
+color_hbase = "#262626"
+color_hhbase = "#363636"
+color_3 = "#a4a4a3"
+color_4 = "#ffffff"
+color_5 = "#ae9ce2"
+color_6 = "#5a9ad9"
+color_button = "#009ddC"
+color_8 = "#ce2d4f"
